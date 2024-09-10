6227f71298818bb784fa2555cc91dc842ed8d2cf
sancheZ250 <sancheZ2502@yandex.ru>
2024-09-10 18:33:30 +0700

    123

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,15 @@ import App from './App.vue';
 import router from './routes';
 import './style.css';
 import axios from 'axios';
-import store from './store';
+import store from './store'; // Подключаем store
 import 'flowbite';
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
+
 axios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = store.getters.getToken; // Берём токен из Vuex store через getter
     if (token) {
       config.headers['Authorization'] = `Token ${token}`;
     }
@@ -20,16 +21,21 @@ axios.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response.status === 401) {
-      localStorage.removeItem('token');
-      // Дополнительные действия, которые вы можете предпринять при истечении токена
+      // Вызываем action logout из store
+      store.dispatch('logout');
+      
+      // Дополнительно можно сделать редирект на страницу логина, если нужно
+      router.push({ name: 'login' }); // 'login' — это имя вашего маршрута логина
     }
     return Promise.reject(error);
   }
 );
+
 app.use(router);
 app.use(store);
 app.mount('#app');
